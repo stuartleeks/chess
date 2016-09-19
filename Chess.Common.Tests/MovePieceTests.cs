de7d73ae59a9ebb5d5c0ef51a513ac0a68677994
Stuart Leeks <stuart@leeks.net>
2016-09-19 22:41:52 +0100

    Add test to move piece and capture

--- Chess.Common.Tests/MovePieceTests.cs
+++ Chess.Common.Tests/MovePieceTests.cs
@@ -10,7 +10,7 @@ namespace Chess.Common.Tests
     public class MovePieceTests
     {
         [Fact]
-        public void MovePawnFromStart()
+        public void MovePawnToEmptySquare()
         {
             //                             0  1  2  3  4  5 6  7
             const string startingBoard = "Br Bk Bb Bq BK Bb Bk Br\r\n" + //0
@@ -38,5 +38,37 @@ namespace Chess.Common.Tests
 
             Assert.Equal(expectedBoard, boardDump);
         }
+
+
+        [Fact]
+        public void MovePawnToTakePiece()
+        {
+            //                             0  1  2  3  4  5 6  7
+            const string startingBoard = "Br Bk Bb Bq BK Bb Bk Br\r\n" + //0
+                                         "Bp Bp Bp    Bp Bp Bp Bp\r\n" + //1
+                                         "                       \r\n" + //2
+                                         "         Bp            \r\n" + //3
+                                         "            Wp         \r\n" + //4
+                                         "                       \r\n" + //5
+                                         "Wp Wp Wp Wp    Wp Wp Wp\r\n" + //6
+                                         "Wr Wk Wb Wq WK Wb Wk Wr\r\n";  //7
+            var board = Board.Parse(startingBoard);
+            board.MovePiece(new[] { 4, 4 }, new[] { 3, 3 });
+
+
+
+            string boardDump = board.Dump();
+            const string expectedBoard = "Br Bk Bb Bq BK Bb Bk Br\r\n" + //0
+                                         "Bp Bp Bp    Bp Bp Bp Bp\r\n" + //1
+                                         "                       \r\n" + //2
+                                         "         Wp            \r\n" + //3
+                                         "                       \r\n" + //4
+                                         "                       \r\n" + //5
+                                         "Wp Wp Wp Wp    Wp Wp Wp\r\n" + //6
+                                         "Wr Wk Wb Wq WK Wb Wk Wr\r\n";  //7
+
+            Assert.Equal(expectedBoard, boardDump);
+        }
+
     }
 }

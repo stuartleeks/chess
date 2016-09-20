b8b9ffa3dad7099747ac37b3d5df253c13244f53
Stuart Leeks <stuart@leeks.net>
2016-09-20 20:34:00 +0100

    basic board

---
+++ Chess.Web/Controllers/GameController.cs
@@ -0,0 +1,26 @@
+﻿using Chess.Web.Models.Game;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Chess.Web.Controllers
+{
+    [RoutePrefix("")]
+    public class GameController : Controller
+    {
+        [Route("")]
+        public ActionResult ShowGame()
+        {
+            var model = new GameModel
+            {
+                Game = new Common.Game
+                {
+                    Board = Common.Board.CreateStartingBoard()
+                }
+            };
+            return View(model);
+        }
+    }
+}
---
+++ Chess.Web/Models/Game/GameModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Common = Chess.Common;
+
+namespace Chess.Web.Models.Game
+{
+    public class GameModel
+    {
+        public Common.Game Game { get; set; }
+    }
+
+    public enum foo
+    {
+
+    }
+}

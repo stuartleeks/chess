036ee07fe6d9f4d90d9b2a53c9f3e30c8264140c
Stuart Leeks <stuart@leeks.net>
2016-09-29 20:41:30 +0100

    Update ssh key handling

--- Deploy/deploy.sh
+++ Deploy/deploy.sh
@@ -3,11 +3,17 @@
 echo load keys...
 
 eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/acs-stuart
+ssh-add /ssh/acs-stuart
 
 # start ssh tunnel
 #kill -kill $(pgrep ssh)
 ssh -L 2375:localhost:2375 -N stuart@slacsswarmmgmt.northeurope.cloudapp.azure.com -p2200 &
+
+exitCode=$?
+if [ $exitCode -ne 0]
+then
+    exit $exitCode
+fi;
 
 export DOCKER_HOST=:2375
 docker-compose pull

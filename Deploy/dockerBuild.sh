07dc01cf31e8394f562986290a75637bc0a8aec6
Stuart Leeks <stuart@leeks.net>
2016-09-29 20:29:50 +0100

    fix up Dockerfile path (again)

--- Deploy/dockerBuild.sh
+++ Deploy/dockerBuild.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build --build-arg BUILD_DATE=`date -u +%Y-%m-%dT%H:%M:%SZ` --build-arg VCS_REF=`git rev-parse --short HEAD` -t stuartleeks/chesstest:$BUILD_BUILDNUMBER .
+docker build --build-arg BUILD_DATE=`date -u +%Y-%m-%dT%H:%M:%SZ` --build-arg VCS_REF=`git rev-parse --short HEAD` -t stuartleeks/chesstest:$BUILD_BUILDNUMBER ..

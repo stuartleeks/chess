28513e7ae2cd9253a7cb02337ba3717eae8c7271
Stuart Leeks <stuart@leeks.net>
2016-09-29 22:33:37 +0100

    fix build arg for BUILDNUMBER

--- Deploy/dockerBuild.sh
+++ Deploy/dockerBuild.sh
@@ -6,4 +6,4 @@ rm -rf $(pwd)/publish
 
 dotnet publish $(pwd)/Chess.Web/project.json -o $(pwd)/publish/chess
 
-docker build --build-arg BUILD_DATE=`date -u +%Y-%m-%dT%H:%M:%SZ` --build-arg VCS_REF=`git rev-parse --short HEAD` --build-arg VCS_REF=$BUILD_BUILDNUMBER -t stuartleeks/chesstest:$BUILD_BUILDNUMBER .
+docker build --build-arg BUILD_DATE=`date -u +%Y-%m-%dT%H:%M:%SZ` --build-arg VCS_REF=`git rev-parse --short HEAD` --build-arg BUILD_NUMBER=$BUILD_BUILDNUMBER -t stuartleeks/chesstest:$BUILD_BUILDNUMBER .

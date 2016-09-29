81d75711fc10062277e087b2ec692ef68bebfb6f
Stuart Leeks <stuart@leeks.net>
2016-09-29 22:00:51 +0100

    Update build and Dockerfile for release

--- Deploy/dockerBuild.sh
+++ Deploy/dockerBuild.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+dotnet restore
+
+rm -rf $(pwd)/publish
+
+dotnet publish $(pwd)/Chess.Web/project.json -o $(pwd)/publish/chess
+
 docker build --build-arg BUILD_DATE=`date -u +%Y-%m-%dT%H:%M:%SZ` --build-arg VCS_REF=`git rev-parse --short HEAD` --build-arg VCS_REF=$BUILD_BUILDNUMBER -t stuartleeks/chesstest:$BUILD_BUILDNUMBER ..

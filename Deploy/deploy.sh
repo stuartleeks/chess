6bbc44bfd02fc231dbd8206b0bf59f50599c3a9c
Stuart Leeks <stuart@leeks.net>
2016-11-21 08:55:59 -0600

    specify identity file in ssh cmd

--- Deploy/deploy.sh
+++ Deploy/deploy.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-echo klil ssh...
-kill -kill $(pgrep ssh-agent)
-kill -kill $(pgrep ssh)
+# echo klil ssh...
+# kill -kill $(pgrep ssh-agent)
+# kill -kill $(pgrep ssh)
 
-echo load keys...
-eval "$(ssh-agent -s)"
-ssh-add /ssh/acs-stuart
+# echo load keys...
+# eval "$(ssh-agent -s)"
+# ssh-add /ssh/acs-stuart
 
-exitCode=$?
-if [ $exitCode -ne 0 ]
-then
-    exit $exitCode
-fi;
+# exitCode=$?
+# if [ $exitCode -ne 0 ]
+# then
+#     exit $exitCode
+# fi;
 
 echo ssh tunnel...
-ssh -L 2375:localhost:2375 -N stuart@slacs-swarmmgmt.northeurope.cloudapp.azure.com -p2200 &
+ssh -L 2375:localhost:2375 -N stuart@slacs-swarmmgmt.northeurope.cloudapp.azure.com -p2200 -i /ssh/acs-stuart &
 
 exitCode=$?
 if [ $exitCode -ne 0 ]

e598d28a3911df62c427b7bc3e3ea697efac7f71
Stuart Leeks <stuart@leeks.net>
2017-01-17 07:31:23 +0000

    revert to swarm

--- Deploy/deploy.sh
+++ Deploy/deploy.sh
@@ -16,9 +16,9 @@ kill -kill $(pgrep ssh)
 
 echo ssh tunnel...
 # TODO parameterise this!
-# ssh -L 2375:localhost:2375 -N stuart@slacs-swarmmgmt.northeurope.cloudapp.azure.com -p2200 -i /ssh/acs-stuart &
 # Turned off StrictHostKeyChecking to simplify setting up new agents to build ;-)
-ssh -L 2375:localhost:2375 -N stuart@slacs-dcosmgmt.northeurope.cloudapp.azure.com -p2200 -i /ssh/acs-stuart -oStrictHostKeyChecking=no &
+ssh -L 2375:localhost:2375 -N stuart@slacs-swarmmgmt.northeurope.cloudapp.azure.com -p2200 -i /ssh/acs-stuart -oStrictHostKeyChecking=no &
+# ssh -L 2375:localhost:2375 -N stuart@slacs-dcosmgmt.northeurope.cloudapp.azure.com -p2200 -i /ssh/acs-stuart -oStrictHostKeyChecking=no &
 
 exitCode=$?
 if [ $exitCode -ne 0 ]

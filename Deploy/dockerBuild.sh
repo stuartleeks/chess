2514f566bcac13d1b60518e33d1b0fa528c779ae
Stuart Leeks <stuart@leeks.net>
2017-03-10 14:01:35 +0000

    Add dotnet apt-get feed

--- Deploy/dockerBuild.sh
+++ Deploy/dockerBuild.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+sudo sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 417A0893
+sudo apt-get update
 sudo apt-get install dotnet-dev-1.0.1
 
 export DOTNET_CLI_TELEMETRY_OPTOUT=1

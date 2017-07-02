253bcfed2e0577f50fd41b80314a442678825377
Stuart Leeks <stuart@leeks.net>
2017-07-02 07:40:38 +0100

    Add debug output

--- Deploy/dockerBuild.sh
+++ Deploy/dockerBuild.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "dockerBuild.sh starting..."
+
 sudo sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 417A0893
 sudo apt-get update

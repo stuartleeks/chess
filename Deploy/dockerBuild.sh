7b7461d8c669161ed66cd9b7350edfa47a3e6425
Stuart Leeks <stuart@leeks.net>
2017-07-02 10:58:28 +0100

    Fix project path

--- Deploy/dockerBuild.sh
+++ Deploy/dockerBuild.sh
@@ -13,6 +13,6 @@ dotnet restore
 
 rm -rf $(pwd)/publish
 
-dotnet publish $(pwd)/Chess.Web/project.json -o $(pwd)/publish/chess
+dotnet publish $(pwd)/Chess.Web/Chess.Web.csproj -o $(pwd)/publish/chess
 
 docker build --build-arg BUILD_DATE=`date -u +%Y-%m-%dT%H:%M:%SZ` --build-arg VCS_REF=`git rev-parse --short HEAD` --build-arg BUILD_BUILDNUMBER=$BUILD_BUILDNUMBER -t stuartleeks/chesstest:$BUILD_BUILDNUMBER .

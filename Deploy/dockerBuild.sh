13faa06465085e82191862d56039cdc8cd12793a
Stuart Leeks <stuart@leeks.net>
2017-07-02 12:12:15 +0100

    Omit dotnet install

--- Deploy/dockerBuild.sh
+++ Deploy/dockerBuild.sh
@@ -2,10 +2,10 @@
 
 echo "dockerBuild.sh starting..."
 
-sudo sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 417A0893
-sudo apt-get update
-sudo apt-get install dotnet-dev-1.0.1
+# sudo sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
+# sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 417A0893
+# sudo apt-get update
+# sudo apt-get install dotnet-dev-1.0.1
 
 export DOTNET_CLI_TELEMETRY_OPTOUT=1
 
